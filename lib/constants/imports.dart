0214e3a3334e10f50af1f964abd1a1bff3362aca
discoveruz <discover.diyorbek@gmail.com>
2021-12-18 15:11:08 +0500

    DRY

--- lib/constants/imports.dart
+++ lib/constants/imports.dart
@@ -6,7 +6,6 @@ export 'package:cash_app/components/text_field_container.dart';
 export 'package:flutter/services.dart';
 export 'package:cash_app/components/rounded_button.dart';
 export 'package:cash_app/Screens/Welcome/welcome_screen.dart';
-
 export 'package:cash_app/models/io_model.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:cash_app/Screens/chiqim/chiqim.dart';
@@ -17,13 +16,11 @@ export 'package:cash_app/home/app_bar_bottom_Section.dart';
 export 'package:cash_app/home/chiqim/chiqim_widget.dart';
 export 'package:cash_app/home/loading_indicator.dart';
 export 'package:cash_app/home/tushum/tushum_widget.dart';
-
 export 'package:cash_app/Screens/Login/components/background.dart';
 export 'package:cash_app/Screens/Signup/signup_screen.dart';
 export 'package:cash_app/components/already_have_an_account_acheck.dart';
 export 'package:cash_app/components/rounded_input_field.dart';
 export 'package:cash_app/components/rounded_password_field.dart';
-
 export 'package:cash_app/Screens/Login/login_screen.dart';
 export 'package:cash_app/components/rounded_phone_input.dart';
 export 'package:cash_app/components/rounded_sms_code_input.dart';
