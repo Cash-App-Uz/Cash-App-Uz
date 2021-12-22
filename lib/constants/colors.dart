446d219a373c8b3a058684174018165e01f038c7
discoveruz <discover.diyorbek@gmail.com>
2021-12-23 00:14:42 +0500

    White and Border color added

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,5 @@ import 'package:cash_app/constants/imports.dart';
 const kAppBar = Color(0xffFF5959);
 const kBackground = Color(0xffEEF2FF);
 const kCategoryWidget = Color(0xff676FA3);
+const kCategoryBorder = Color(0xffCDDEFF);
+const kWhite = Color(0xffffffff);
