80b2f138667354e87d0f00cd9f9aba21b493422f
Samandar Abduhamitov <samandarabduhamitov@gmail.com>
2021-12-22 22:16:17 +0500

    chiqim sahifasiga clear qo'shildi

--- lib/Screens/chiqim/chiqim.dart
+++ lib/Screens/chiqim/chiqim.dart
@@ -245,6 +245,7 @@ class _CreatBudgetPageState extends State<CreatBudgetPage> {
                 Paths().userInfo,
               );
               budgetPrice.clear();
+              budgetName.clear();
             },
           )
         ],
